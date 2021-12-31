49955edafd03abb915ad5d3e4327ec2b6fa6c65d
pananon <pandeyjss01@gmail.com>
2021-12-31 17:22:21 +0530

    Added DarkMode feature/Made website responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,14 @@
-import React,{useRef,useState} from 'react'
+import React,{useRef,useState,useContext} from 'react'
 import './contact.css'
 import Phone from '../../images/phone.png'
 import Email from '../../images/email.png'
 import Address from '../../images/address.png'
 import emailjs from 'emailjs-com';
+import {ThemeContext} from '../../context'
 
 function Contact() {
+    const theme = useContext(ThemeContext)
+    const darkMode=theme.state.darkMode;
     const formRef = useRef()
     const [done, setDone] = useState(1)
     const handleSubmit=(e)=>{
@@ -45,10 +48,10 @@ function Contact() {
                         Let's get connected
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder='Name' name="user_name" />
-                        <input type="text" placeholder='Subject' name="user_subject" />
-                        <input type="text" placeholder='Email' name="user_email" />
-                        <textarea rows="5" placeholder='Message' name="message"></textarea>
+                        <input style={{backgroundColor:darkMode?'#333':'white'}} type="text" placeholder='Name' name="user_name" />
+                        <input style={{backgroundColor:darkMode?'#333':'white'}} type="text" placeholder='Subject' name="user_subject" />
+                        <input style={{backgroundColor:darkMode?'#333':'white'}} type="text" placeholder='Email' name="user_email" />
+                        <textarea style={{backgroundColor:darkMode?'#333':'white'}} rows="5" placeholder='Message' name="message"></textarea>
                         <button>Submit</button>
                         {done===2?"Thank you for contacting":""}
                         {done===3?"There were some error and we couldn't recieve your mail":""}
